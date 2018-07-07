fd44b304b59f1eb8dfc165825ffea7f3d3113934
MAURO FELIPE <mauro.felipe@fatec.sp.gov.br>
2018-07-07 12:03:29 -0300

    versao 6

--- ContatoWeb/Controllers/ContatoController.cs
+++ ContatoWeb/Controllers/ContatoController.cs
@@ -11,17 +11,30 @@ namespace ContatoWeb.Controllers
 {
     public class ContatoController : ApiController
     {
-        private SqlConnection conn = new SqlConnection("Server=tcp:aualposmauro.database.windows.net,1433;Initial Catalog=bdcontato;Persist Security Info=False;User ID=mauro;Password=H1per_tec;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
+        private SqlConnection conn = new SqlConnection("Server=tcp:aulaposmauro2.database.windows.net,1433;Initial Catalog=bdcontato2;Persist Security Info=False;User ID=mauro;Password=a1a2a3a4a5a6_;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
         
         // Teste
         public string Get()
         {
+            var lista = new List<Contato>();
+
             conn.Open();
             SqlCommand cmd = new SqlCommand();
             cmd.Connection = conn;
-            cmd.CommandText = @"select * from contato for json auto";
+            cmd.CommandText = @"select * from contato";
 
             SqlDataReader reader = cmd.ExecuteReader();
+
+            while (reader.Read())
+            {
+                Contato c = new Contato()
+                {
+                    ContatoId = (int)reader["ContatoId"],
+                    Nome = (string)reader["Nome"],
+                    Email = (string)reader["Email"]
+                };
+                lista.Add(c);
+            }
 
             conn.Close();
             return reader.GetString(0);
